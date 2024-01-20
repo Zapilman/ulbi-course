de2f855db66c686916e853845858259998c1a944
Zapilman <17uraz12@gmail.com>
2024-01-20 14:43:18 +0200

    add hmr

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -3,6 +3,7 @@ import webpack from "webpack";
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 import getCommonConfig from "./config/webpack/webpack.config";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export default merge(getCommonConfig(__dirname), {
   mode: "development",
@@ -11,8 +12,11 @@ export default merge(getCommonConfig(__dirname), {
     port: 3000,
     open: true,
     historyApiFallback: true,
+    hot: true,
   },
   plugins: [
+    new ReactRefreshWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.ProgressPlugin(),
     new ForkTsCheckerWebpackPlugin({
       typescript: {
